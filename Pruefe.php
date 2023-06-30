18bb369176984aa4ad2cd32a859fa69b31768e31
Sascha Dierl <saschdierl05@gmail.com>
2023-06-30 10:34:44 +0200

    Dateien vorbereitet mit html

---
+++ Pruefe.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <title>Püfe</title>
+    <link rel="stylesheet" type="text/css" href="stylesheets/main.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets/reset.css">
+    <script defer src="Funktionen.js"></script>
+
+    <?php
+    // Php hier
+    ?>
+  </head>
+  <body>
+    <header>
+      <h1>Reiseverwaltung</h1>
+    </header>
+    <main class="c-vertical c-horizontal">
+    </main>
+    <footer>
+      <p>© 2023 Reiseverwaltung GmbH</p>
+      <p>© 2023 von Firmenname GmbH</p>
+    </footer>
+  </body>
+</html>
+  <script>
+    // javascript hier
+  </script>
